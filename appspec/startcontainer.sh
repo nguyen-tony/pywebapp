64e566a80949d736cc080ede265dbc764beb1bae
nguyen-tony <32149412+nguyen-tony@users.noreply.github.com>
2019-11-07 12:30:21 -0800

    Update startcontainer.sh

--- appspec/startcontainer.sh
+++ appspec/startcontainer.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-$(aws ecr get-login --region us-west-2)
-docker pull 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest || {
+$(aws ecr get-login --no-include-email --region us-west-2)
+docker pull 734502624332.dkr.ecr.us-west-2.amazonaws.com/nguyen-tony/pywebapp:latest || {
     echo "ERROR: docker pull failed. Sleeping for 10 minutes to allow investigation..."
     sleep 600
     exit 1
 }
-docker run --name pywebapp -p 80:8080 --detach 203417976784.dkr.ecr.us-west-2.amazonaws.com/comp4913/pywebapp:latest
+docker run --name pywebapp -p 80:8080 --detach 734502624332.dkr.ecr.us-west-2.amazonaws.com/nguyen-tony/pywebapp:latest
